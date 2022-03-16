7675cb2696d02b19ac3ed333d3f530888f132363
Keizo17 <95151633+Keizo17@users.noreply.github.com>
2022-03-16 08:10:00 -0400

    Added code in package format

---
+++ Code/logic/customerRecord.java
@@ -0,0 +1,47 @@
+package logic;
+import java.util.Scanner;
+
+
+public class customerRecord{
+
+    private String fName, lName, email, pNum, address, id;
+    private int age;
+    private Scanner scanner = new Scanner(System.in);  
+
+    public customerRecord(String empty){
+        this.id = empty;
+    }
+
+    public customerRecord(String fName, String lName, int age, String address, String phone, String email, String id)
+    {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.pNum = phone;
+        this.address = address;
+        this.age = age;
+        this.id = id;
+    }
+    public String getfName() {
+        return this.fName;
+    }
+    public String getlName() {
+        return this.lName;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public String getPhone() {
+        return this.pNum;
+    }
+    public String getAddress() {
+        return this.address;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public String getID() {
+        return this.id;
+    }
+
+}
